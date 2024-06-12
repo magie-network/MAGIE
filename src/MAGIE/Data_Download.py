35800a785b6b51b18834c0fe3c1a33aaf53b6dfe
simon_work <simon_james_walker@hotmail.co.uk>
2024-06-12 11:55:26 +0100

    Doc string and comments added

--- src/MAGIE/Data_Download.py
+++ src/MAGIE/Data_Download.py
@@ -8,105 +8,153 @@ from General_Tools.user_input_tools import validinput
 import warnings
 from progressbar import progressbar
 from pandas.errors import ParserError
+
 def Download_MAGIE(start, end, sites=['arm', 'dun', 'val'], save_file_name=False):
-    url_prefix= 'https://data.magie.ie/'
-    #check for earliest year
-    min_year=0
+    """
+    Downloads MAGIE data for specified sites and date range, and saves it to a file.
+
+    Parameters:
+    start (numpy.datetime64): The start date of the data to be downloaded.
+    end (numpy.datetime64): The end date of the data to be downloaded.
+    sites (list of str, optional): List of site codes to download data from. Defaults to ['arm', 'dun', 'val'].
+    save_file_name (str or bool, optional): Filename to save the downloaded data. If False, a default name will be generated. Defaults to False.
+
+    Raises:
+    ValueError: If the start or end date is out of range of the available data.
+    FileExistsError: If the specified save file already exists and the user chooses not to append to it.
+    
+    Returns:
+    str: The name of the file where the data is saved.
+    """
+    url_prefix = 'https://data.magie.ie/'
+    
+    # Check for the earliest available year
+    min_year = 0
     for i in range(2000, dt.now().year, 1):
-        if requests.get(f"{url_prefix}{i}/").status_code<400:
-            min_year= i
+        if requests.get(f"{url_prefix}{i}/").status_code < 400:
+            min_year = i
             break
-    # Check if first year was found
+    
+    # Raise an error if no year is found
     if not min_year:
         raise ValueError(f'Unable to find the first year available in the range 2000-{dt.now().year}')
-    #check for latest year
-    max_year=0
+    
+    # Check for the latest available year
+    max_year = 0
     for i in range(dt.now().year, 2000, -1):
-        if requests.get(f"{url_prefix}{i}/").status_code<400:
-            max_year= i
+        if requests.get(f"{url_prefix}{i}/").status_code < 400:
+            max_year = i
             break
-    # Check if last year was found
+    
+    # Raise an error if no year is found
     if not max_year:
         raise ValueError(f'Unable to find the last year available in the range 2000-{dt.now().year}')
-    # Check if start date is valid
-    if start<np.datetime64(f'{min_year}-01-01'):
-        warnings.warn(f'start time is less than the first year available from site: {min_year}.'\
-                            'The download will begin at the first year available')
-        start= np.datetime64(f'{min_year}-01-01')
-    # Check if end date is valid
-    if end>np.datetime64(f'{max_year}-12-31'):
-        warnings.warn(f'end time is greater than the last year available from site: {max_year}.'\
-                            'The download will end at the last year available')
-        end= np.datetime64(f'{max_year}-12-31')
-    # Create Filename if not specified
+    
+    # Adjust the start date if it is earlier than the first available year
+    if start < np.datetime64(f'{min_year}-01-01'):
+        warnings.warn(f'Start time is less than the first year available from site: {min_year}. '
+                      'The download will begin at the first year available.')
+        start = np.datetime64(f'{min_year}-01-01')
+    
+    # Adjust the end date if it is later than the last available year
+    if end > np.datetime64(f'{max_year}-12-31'):
+        warnings.warn(f'End time is greater than the last year available from site: {max_year}. '
+                      'The download will end at the last year available.')
+        end = np.datetime64(f'{max_year}-12-31')
+    
+    # Create a default filename if none is provided
     if not save_file_name:
-        save_file_name= '_'.join(sites)+f'{date2filename(start)[:-9]}_to_{date2filename(end)[:-9]}.hdf5'
+        save_file_name = '_'.join(sites) + f'{date2filename(start)[:-9]}_to_{date2filename(end)[:-9]}.hdf5'
+    
+    # Check if the save file already exists
     if os.path.isfile(save_file_name):
-        if not validinput('save file: {save_file_name} already exists append to pre-existing file? (y/n)', 
-                      'y', 'n'):
-            raise FileExistsError('Please either provide a new save file path using "save_file_name" argument'\
+        if not validinput(f'save file: {save_file_name} already exists. Append to pre-existing file? (y/n)', 'y', 'n'):
+            raise FileExistsError('Please either provide a new save file path using "save_file_name" argument '
                                   'or delete existing file and rerun')
         else:
             warnings.warn(f'Appending to pre-existing file: {save_file_name}', UserWarning)
-    columns= ['Date_UTC', 'Index', 'Bx', 'By', 'Bz', 'E1', 'E2', 'E3', 'E4', 'TFG','TE', 'Volts']
-    drop_index= columns.copy()
-    # drop_index.remove('Index')
-    drop_index[1]='Site'
-    # Construct array of dates to download
-    dates= np.array([start + np.timedelta64(i, 'D') for i in \
-                     range((end-start).astype('timedelta64[D]').astype(int)+1)])
-    for date in progressbar(dates,max_value=len(dates)):
-        date= date.astype('datetime64[D]').astype(str).split('-')
-        url= url_prefix + '{}/{}/{}/txt/'.format(*date)
+    
+    # Define column names for the data
+    columns = ['Date_UTC', 'Index', 'Bx', 'By', 'Bz', 'E1', 'E2', 'E3', 'E4', 'TFG', 'TE', 'Volts']
+    drop_index = columns.copy()
+    drop_index[1] = 'Site'
+    
+    # Construct an array of dates to download
+    dates = np.array([start + np.timedelta64(i, 'D') for i in range((end - start).astype('timedelta64[D]').astype(int) + 1)])
+    
+    # Loop through each date and download the data
+    for date in progressbar(dates, max_value=len(dates)):
+        date = date.astype('datetime64[D]').astype(str).split('-')
+        url = url_prefix + '{}/{}/{}/txt/'.format(*date)
+        # loop through each site
         for site in sites:
-            filename= site+'{}{}{}.txt'.format(*date)
-            if requests.get(f"{url}{filename}").status_code>=400:
-                warnings.warn(f'File not found for site= {site} on '+'{}-{}-{}'.format(*date[::-1]))
+            filename = site + '{}{}{}.txt'.format(*date)
+            
+            # Check if the file exists on the server
+            if requests.get(f"{url}{filename}").status_code >= 400:
+                warnings.warn(f'File not found for site= {site} on ' + '{}-{}-{}'.format(*date[::-1]))
                 continue
+            
+            # Download the file using wget
             os.system(f'wget {url}{filename}')
+            
+            # File bug handling for empty tabs appearing on some lines
             try:
-                file= pd.read_csv(filename, delimiter='\t', 
-                                names=columns, 
-                                skiprows=1, parse_dates=['Date_UTC'], dayfirst=True, index_col=False).replace(99.99999e3, np.nan)
-                file['Site']= [site]*len(file)
+                # Try to read the file into a DataFrame
+                file = pd.read_csv(filename, delimiter='\t', 
+                                   names=columns, 
+                                   skiprows=1, parse_dates=['Date_UTC'], dayfirst=True, index_col=False).replace(99.99999e3, np.nan)
+                file['Site'] = [site] * len(file)
             except ParserError:
-                F= open(filename, mode='r')
-                f= F.read()
-                new_f='\n'.join(f.split('\t\n'))
-                F.close()
-                F= open(filename, mode='w')
-                F.write(new_f)
-                F.close()
+                # Handle ParserError by modifying the file content and re-reading it
+                with open(filename, mode='r') as F:
+                    f = F.read()
+                new_f = '\n'.join(f.split('\t\n'))
+                with open(filename, mode='w') as F:
+                    F.write(new_f)
+                
                 if os.path.isfile('badfiles.txt'):
                     with open('bad_files.txt', 'r') as f:
-                        bad_files= f.read()
-                        f.close()
+                        bad_files = f.read()
                 else:
-                    bad_files=''
-                bad_files+='parser error:'+ filename+'\n'
+                    bad_files = ''
+                
+                bad_files += 'parser error:' + filename + '\n'
+                
                 with open('bad_files.txt', 'w') as f:
                     f.write(bad_files)
-                    f.close()
-                file= pd.read_csv(filename, delimiter='\t', 
-                                names=columns, 
-                                skiprows=1, parse_dates=['Date_UTC'], dayfirst=True, index_col=False).replace(99.99999e3, np.nan)
-                file['Site']= [site]*len(file)
+                
+                file = pd.read_csv(filename, delimiter='\t', 
+                                   names=columns, 
+                                   skiprows=1, parse_dates=['Date_UTC'], dayfirst=True, index_col=False).replace(99.99999e3, np.nan)
+                file['Site'] = [site] * len(file)
+            
+            # File bug handling when last line of the file contains incomplete data points
+            # Check for object data types and handle them appropriately
             if 'O' in [file[col].dtype for col in file.columns[:-1]]:
                 if os.path.isfile('bad_files.txt'):
                     with open('bad_files.txt', 'r') as f:
-                        bad_files= f.read()
-                        f.close()
+                        bad_files = f.read()
                 else:
-                    bad_files=''
-                bad_files+=filename+'\n'
+                    bad_files = ''
+                
+                bad_files += filename + '\n'
+                
                 with open('bad_files.txt', 'w') as f:
                     f.write(bad_files)
-                    f.close()
-                file= file.loc[0:len(file)-2]
-                if file['Date_UTC'].dtype=='O':
-                    file['Date_UTC']= pd.to_datetime(file.Date_UTC, dayfirst=True)
+                
+                file = file.loc[0:len(file)-2]
+                
+                if file['Date_UTC'].dtype == 'O':
+                    file['Date_UTC'] = pd.to_datetime(file.Date_UTC, dayfirst=True)
+                
                 for column in columns[2:]:
-                    if file[column].dtype=='O':
-                        file[column]= file[column].astype('float64')
-            file[drop_index].to_hdf(save_file_name, key='main',mode='a',append=True,format='t', data_columns=True)
+                    if file[column].dtype == 'O':
+                        file[column] = file[column].astype('float64')
+            
+            # Save the data to an HDF5 file using specified columns (the index column is removed as it is meaningless in a merged file)
+            file[drop_index].to_hdf(save_file_name, key='main', mode='a', append=True, format='t', data_columns=True)
+            
+            # Remove the downloaded file to save space
             os.remove(filename)
+            return save_file_name
