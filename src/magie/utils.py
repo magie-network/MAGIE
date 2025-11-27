545824a80af071e9bc668ae9bf52627955d5cbd8
simon_work <simon_james_walker@hotmail.co.uk>
2025-11-27 11:20:19 +0000

    moved validinput to utils

--- src/magie/utils.py
+++ src/magie/utils.py
@@ -46,3 +46,13 @@ def enforce_types(**type_map):
         return wrapper
 
     return decorator
+# Handling import errors for GitHub repositories
+def validinput(inputstr, positive_answer, negative_answer):
+    answer = input(inputstr+'\n')
+    if answer == positive_answer:
+        return True
+    elif answer == negative_answer:
+        return False
+    else:
+        print('Invalid response should be either' + str(positive_answer) + ' or ' + str(negative_answer))
+        return validinput(inputstr, positive_answer, negative_answer)
