19d05ec99b5b267284c890c9244a85c9db947204
simon_work <simon_james_walker@hotmail.co.uk>
2026-04-09 16:29:55 +0100

    changed from magie k index code to magpy

--- src/magie/__init__.py
+++ src/magie/__init__.py
@@ -1,11 +1,12 @@
-from .k_index import provisional_k, finalised_k, live_k, plot_k
+# from .k_index import provisional_k, finalised_k, live_k, plot_k
+from .k_index_magpy import live_k, plot_k
 from.Data_Download import download_magie
 
 
 __all__ = [
     "download_magie",
     "live_k",
-    "provisional_k",
-    "finalised_k",
+    # "provisional_k",
+    # "finalised_k",
     "plot_k"
 ]
