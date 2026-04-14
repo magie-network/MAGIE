ea98f3407e79a58f3228ba90e92e581b1dc6f633
simon_work <simon_james_walker@hotmail.co.uk>
2026-04-14 16:29:13 +0100

    added legacy alert to core module

--- src/magie/__init__.py
+++ src/magie/__init__.py
@@ -1,6 +1,7 @@
 # from .k_index import provisional_k, finalised_k, live_k, plot_k
 from .k_index_magpy import live_k, plot_k
 from .Data_Download import download_magie
+from .legacy_alert import alert
 
 
 __all__ = [
@@ -8,5 +9,6 @@ __all__ = [
     "live_k",
     # "provisional_k",
     # "finalised_k",
-    "plot_k"
+    "plot_k",
+    "alert"
 ]
