ab336d7b22da012f99a548515bb96c7fd80e6e62
simon_work <simon_james_walker@hotmail.co.uk>
2026-04-15 15:43:56 +0100

    cleaned unnecessary text

--- live_scripts/daily_k.py
+++ live_scripts/daily_k.py
@@ -16,16 +16,16 @@ if __name__ == "__main__": # daily update of archive
     import pandas as pd
     now_time= pd.Timestamp("2026-01-01").floor("1D")
     for site_code in ["val", "dun"]:
-        kvals, errors = daily_K_full_archive(site_code=site_code, archive_path_builder=lambda date: "/home/simon/Documents/magnetometer_archive/{}/{}/{}/iaga2002/".format(
+        kvals, errors = daily_K_full_archive(site_code=site_code, archive_path_builder=lambda date: "/magnetometer_archive/{}/{}/{}/iaga2002/".format(
             *date
         ),
-        output_path_builder=lambda date: "/home/simon/Documents/magnetometer_archive/{}/{}/{}/k_index/".format(
+        output_path_builder=lambda date: "/magnetometer_archive/{}/{}/{}/k_index/".format(
             *date
         ), start=now_time-pd.Timedelta(3, 'D'), end=now_time-pd.Timedelta(2, 'D'), max_workers=8, error_log_path="./daily_k_errors.log")
         print(kvals)
         print(errors)
-        results, errors = daily_K_plots_full_archive(site_code=site_code, archive_path_builder=lambda date: "/home/simon/Documents/magnetometer_archive/{}/{}/{}/k_index/".format(
-            *date), output_path_builder=lambda date: "/home/simon/Documents/magnetometer_archive/{}/{}/{}/k_index/".format(
+        results, errors = daily_K_plots_full_archive(site_code=site_code, archive_path_builder=lambda date: "/magnetometer_archive/{}/{}/{}/k_index/".format(
+            *date), output_path_builder=lambda date: "/magnetometer_archive/{}/{}/{}/k_index/".format(
             *date), start=now_time-pd.Timedelta(3, 'D'), end=now_time-pd.Timedelta(2, 'D'), max_workers=8, error_log_path="./daily_k_errors.log")
         print(results)
         print(errors)
