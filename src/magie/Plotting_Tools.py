7411938d3ef5e1dffa5846e973fcca83b7de1e12
simon_work <simon_james_walker@hotmail.co.uk>
2026-06-05 15:12:34 +0100

    added filter option and rescaled pad

--- src/magie/Plotting_Tools.py
+++ src/magie/Plotting_Tools.py
@@ -332,7 +332,7 @@ def contour_labels(contour, xpad=None, ypad=None, sides=['left', 'right'], rtol=
     show_logo=bool,
     auto_xlim=bool,
 )
-def plot_BxByBz(data, logo_path=None, show_logo= False, auto_xlim=True):
+def plot_BxByBz(data, logo_path=None, show_logo= False, auto_xlim=True, figsize_scale=3, filter=True):
     """
     Plot the X, Y, and Z magnetic field components as stacked time series.
 
@@ -363,10 +363,11 @@ def plot_BxByBz(data, logo_path=None, show_logo= False, auto_xlim=True):
     import matplotlib.dates as mdates
 
     data = data.copy()  # avoid mutating caller data
-    data= data.filter()
+    if filter:
+        data= data.filter()
 
     # Use one shared figure with vertically stacked axes for component comparison.
-    fig= plt.figure(figsize=(400/96, 378/96), dpi=96)
+    fig= plt.figure(figsize=(400/96 * figsize_scale, 378/96 * figsize_scale), dpi=96)
     gs= fig.add_gridspec(3, 1, hspace=0.2)
     ax_Bx= fig.add_subplot(gs[0])
     ax_By= fig.add_subplot(gs[1])
@@ -429,11 +430,11 @@ def plot_BxByBz(data, logo_path=None, show_logo= False, auto_xlim=True):
     # Show hour labels only on the bottom panel.
     ax_Bz.xaxis.set_minor_locator(mdates.HourLocator(byhour=[0, 6, 12, 18]))
     ax_Bz.xaxis.set_minor_formatter(mdates.DateFormatter('%H'))
-    ax_Bz.tick_params(axis='x', which='major', labelrotation=0, pad=15)
-    ax_Bz.tick_params(axis='x', which='minor', labelrotation=0, pad=2)
+    ax_Bz.tick_params(axis='x', which='major', labelrotation=0, pad=15*figsize_scale)
+    ax_Bz.tick_params(axis='x', which='minor', labelrotation=0, pad=2*figsize_scale)
     for ax in [ax_Bx, ax_By, ax_Bz]:
-        ax.tick_params(axis='both', labelsize=11)
-        ax.yaxis.label.set_size(11)
+        ax.tick_params(axis='both', which='both', labelsize=10.5* figsize_scale)
+        ax.yaxis.label.set_size(10.5* figsize_scale)
 
     return fig, ax_Bx, ax_By, ax_Bz
 
@@ -443,7 +444,7 @@ def plot_BxByBz(data, logo_path=None, show_logo= False, auto_xlim=True):
     show_logo=bool,
     auto_xlim=bool,
 )
-def plot_dH(data, logo_path=None, show_logo= False, auto_xlim=True):
+def plot_dH(data, logo_path=None, show_logo= False, auto_xlim=True, figsize_scale=3, filter=True):
     """
     Plot declination, horizontal intensity, and the first difference of H.
 
@@ -474,10 +475,11 @@ def plot_dH(data, logo_path=None, show_logo= False, auto_xlim=True):
     import matplotlib.dates as mdates
 
     data = data.copy()  # avoid mutating caller data
-    data= data.filter()
+    if filter:
+        data= data.filter()
 
     # Use one shared figure with vertically stacked axes for derived quantities.
-    fig= plt.figure(figsize=(400/96, 378/96), dpi=96)
+    fig= plt.figure(figsize=(400/96 * figsize_scale, 378/96 * figsize_scale), dpi=96)
     gs= fig.add_gridspec(3, 1, hspace=0.2)
     ax_D= fig.add_subplot(gs[0])
     ax_H= fig.add_subplot(gs[1])
@@ -557,8 +559,8 @@ def plot_dH(data, logo_path=None, show_logo= False, auto_xlim=True):
     ax_dH.xaxis.set_minor_formatter(mdates.DateFormatter('%H'))
 
 
-    ax_dH.tick_params(axis='x', which='major', labelrotation=0, pad=15)
-    ax_dH.tick_params(axis='x', which='minor', labelrotation=0, pad=2)
+    ax_dH.tick_params(axis='x', which='major', labelrotation=0, pad=15*figsize_scale)
+    ax_dH.tick_params(axis='x', which='minor', labelrotation=0, pad=2*figsize_scale)
     ax_D.set_ylabel('D (degrees)')
     ax_H.set_ylabel('H (nT)')
     ax_dH.set_ylabel(r'$\frac{dH}{dt}$ (nT/min)')
@@ -566,6 +568,6 @@ def plot_dH(data, logo_path=None, show_logo= False, auto_xlim=True):
     # Keep positive and negative dH excursions visually comparable.
     ax_dH.set_ylim(-ymax, ymax)
     for ax in [ax_D, ax_H, ax_dH]:
-        ax.tick_params(axis='both', labelsize=11)
-        ax.yaxis.label.set_size(11)
+        ax.tick_params(axis='both', which='both', labelsize=10.5* figsize_scale)
+        ax.yaxis.label.set_size(10.5* figsize_scale)
     return fig, ax_D, ax_H, ax_dH
